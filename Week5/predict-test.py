622e183f0f73c0548e5858bce1829001b1e0263b
Cecile Guillot <cecile@guillot-salmacis.com>
2023-10-04 16:40:26 +0200

    add virtual env with pipenv

---
+++ Week5/predict-test.py
@@ -0,0 +1,33 @@
+import requests
+
+url = 'http://localhost:9696/predict'
+
+customer = {
+  "customerid": "8879-zkjof",
+  "gender": "female",
+  "seniorcitizen": 0,
+  "partner": "no",
+  "dependents": "no",
+  "tenure": 41,
+  "phoneservice": "yes",
+  "multiplelines": "no",
+  "internetservice": "dsl",
+  "onlinesecurity": "yes",
+  "onlinebackup": "no",
+  "deviceprotection": "yes",
+  "techsupport": "yes",
+  "streamingtv": "yes",
+  "streamingmovies": "yes",
+  "contract": "one_year",
+  "paperlessbilling": "yes",
+  "paymentmethod": "bank_transfer_(automatic)",
+  "monthlycharges": 79.85,
+  "totalcharges": 3320.75
+}
+response = requests.post(url, 
+                         json=customer)
+response.raise_for_status()
+print(response.json())
+
+if response['churn'] == True:
+    print(f"sending promo email to {customer['customerid']}")
